3eb5da8878eb8c08a7ff8a0aa501edd6a3ee5b91
Tevin Joel Aduma <tevin74@live.com>
2017-02-27 12:26:25 +0300

    finalize webpage to show full names of every contact

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //business logic
 function Contact(first, last) {
     this.firstName = first;
-    this.secondName = last;
+    this.lastName = last;
 }
 
 //user interface logic
-$(document).ready(function(){
+$(document).ready(function() {
     $("form#new-contact").submit(function(event) {
         event.preventDefault();
 
@@ -14,10 +14,18 @@ $(document).ready(function(){
 
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>"+newContact.firstName+"</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
+
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+        });
+
 
     });
 });
