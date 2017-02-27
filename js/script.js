35c430727a31fbb548395d03fdce3f1110ffe343
Tevin Joel Aduma <tevin74@live.com>
2017-02-27 11:17:19 +0300

    begin user interface logic

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ function Contact(first, last) {
     this.firstName = first;
     this.secondName = last;
 }
+
+//user interface logic
+$document.ready(function){
+    $("form#new-contact").submit((function) {
+        event.preventDefault();
+
+        var inputtedFirstName = $("input#new-first-name").val();
+        var inputtedLastName = $("input#new-last-name").val();
+
+        var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+        
+    }
+}
